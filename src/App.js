315e9869f76adec9c498696d5ad8cbfd7bef112e
Ashish Sharda <ashishjsharda@gmail.com>
2021-11-30 05:12:01 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,9 +6,12 @@ import ExpenseItem from './components/ExpenseItem'
 function App() {
   return (
    <div>
-     <h2> Let's get started</h2>
-       <p> This is a test paragraph</p>
-       <ExpenseItem></ExpenseItem>
+     <div>
+         <h2>Title</h2>
+     </div>
+       <div>
+           Summary
+       </div>
    </div>
   );
 }
