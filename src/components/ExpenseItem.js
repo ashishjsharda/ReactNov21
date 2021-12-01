b19b4d462d689c85e5ffa6949b6d0b26fe40d900
Ashish Sharda <ashishjsharda@gmail.com>
2021-12-01 05:42:30 -0500

    Add class

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,6 +3,7 @@ function ExpenseItem() {
 
     return <div className={"expense-item"}>
         <div> Dec 1 2021</div>
+        <div className ="Ttitle"> Title of this page is Expense</div>
     </div>
 }
 
