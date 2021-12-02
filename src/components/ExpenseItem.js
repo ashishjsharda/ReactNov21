4f34e79da4222394f580104f20efc31da5a819cd
Ashish Sharda <ashishjsharda@gmail.com>
2021-12-02 06:07:28 -0500

    Update ExpenseItem.js add price

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -5,6 +5,7 @@ function ExpenseItem() {
         <div> Dec 1 2021</div>
         <div className ="expense-item__description">
             <h2> Car Insurance</h2>
+            <div className={"expense-item__price"}>$500</div>
         </div>
     </div>
 }
