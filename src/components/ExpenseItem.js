91e9081602ffdc660671dc99f21e8a918dd5f5ac
Ashish Sharda <ashishjsharda@gmail.com>
2021-12-02 06:02:12 -0500

    Code cleanup

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,7 @@
 import './ExpenseItem.css'
 function ExpenseItem() {
 
-    return <div className={"expense-item"}>
+    return <div className="expense-item">
         <div> Dec 1 2021</div>
         <div className ="Ttitle"> Title of this page is Expense</div>
     </div>
