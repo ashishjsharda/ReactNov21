04116c7336ce36988ce17757b1c8052f86c238b7
Ashish Sharda <ashishjsharda@gmail.com>
2021-12-02 06:03:20 -0500

    Update ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,7 +3,9 @@ function ExpenseItem() {
 
     return <div className="expense-item">
         <div> Dec 1 2021</div>
-        <div className ="Ttitle"> Title of this page is Expense</div>
+        <div className ="expense-item__description">
+            <h2> Car Insurance</h2>
+        </div>
     </div>
 }
 
