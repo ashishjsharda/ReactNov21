4f1dd8d1a310a99ce32e7fea96334ba90da88e37
Ashish Sharda <ashishjsharda@gmail.com>
2021-12-03 05:20:13 -0500

    Add consts

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,10 @@
 import './ExpenseItem.css'
 function ExpenseItem() {
+    const expenseDate= new Date(
+        2021,12,3)
 
     return <div className="expense-item">
-        <div> Dec 1 2021</div>
+        <div> {expenseDate}</div>
         <div className ="expense-item__description">
             <h2> Car Insurance</h2>
             <div className={"expense-item__price"}>$500</div>
