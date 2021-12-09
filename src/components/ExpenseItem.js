ca73f7396008bb6a799c6126391c0729cd9d1274
Ashish Sharda <ashishjsharda@gmail.com>
2021-12-09 06:15:48 -0500

    Update ExpenseItem

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,12 @@
 import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate'
 function ExpenseItem(props) {
     const expenseDate= new Date(
         2021,12,3)
     const summary="Car Insurance"
 
     return <div className="expense-item">
-        <div>
-            <div>{props.date.toLocaleString('en-US',{month:'long'})}</div>
-            <div>{props.date.toLocaleString('en-US',{day:'2-digit'})}</div>
-            <div>{props.date.getFullYear()}</div>
-        </div>
+        <ExpenseDate />
         <div className ="expense-item__description">
             <h2> {props.title}</h2>
             <div className={"expense-item__price"}>{props.amount}</div>
