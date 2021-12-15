32c5dc0af9e60045826a63df07fd565633c74967
Ashish Sharda <ashishjsharda@gmail.com>
2021-12-15 05:00:34 -0500

    Update classnames

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,3 +1,4 @@
+import './ExpenseDate.css'
 function ExpenseDate(props){
     const month=props.date.toLocaleString('en-US',{month:'long'})
     const day=props.date.toLocaleString('en-US',{day:'2-digit'})
@@ -6,9 +7,9 @@ function ExpenseDate(props){
     
     return (
         <div className="expense-date">
-            <div>{props.date.toLocaleString('en-US',{month:'long'})}</div>
-            <div>{props.date.toLocaleString('en-US',{day:'2-digit'})}</div>
-            <div>{props.date.getFullYear()}</div>
+            <div className="expense_date_month">{props.date.toLocaleString('en-US',{month:'long'})}</div>
+            <div className="expense_date_day">{props.date.toLocaleString('en-US',{day:'2-digit'})}</div>
+            <div className="expense_date_year">{props.date.getFullYear()}</div>
         </div>
 
         );
