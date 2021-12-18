c7e117902c5026fba7d2a5566f8fee04e9627105
Ashish Sharda <ashishjsharda@gmail.com>
2021-12-18 06:22:22 -0500

    add props

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,3 +1,17 @@
-function Expenses(){
+import ExpenseItem from "./ExpenseItem";
 
+function Expenses(props){
+
+    return <div>
+        <ExpenseItem
+            date={expenses[0].date}
+            title={expenses[0].title}
+            amount={expenses[0].amount} >
+        </ExpenseItem>
+        <ExpenseItem
+            date={expenses[1].date}
+            title={expenses[1].title}
+            amount={expenses[1].amount}>
+        </ExpenseItem>
+    </div>
 }
